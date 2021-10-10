0229b6ac93d32134a1c5041019f7c1b945b3de21
bombaybb <bombayapirat@gmail.com>
2021-10-10 21:50:09 +0100

    working navbar and added reveal animation

--- components/Header.js
+++ components/Header.js
@@ -1,88 +1,122 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment } from "react";
 import Logo from "../public/images/logo.png";
 import React, { useState } from "react";
-import { Popover } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
-const NavItem = (props) => (
-  // Menu list items
-  <li className="flex items-center justify-center">
-    <Link href={props.href}>
-      <a className="text-base font-medium text-gray-800 px-2 py-1 hover:bg-gray-100 rounded transition-colors">
-        {props.text}
-      </a>
-    </Link>
-  </li>
-);
+const navigation = [
+  { name: "Product", href: "#", current: true },
+  { name: "Solution", href: "#", current: false },
+  { name: "Resources", href: "#", current: false },
+  { name: "Download", href: "#", current: false },
+  { name: "Pricing", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function Header() {
   return (
-    <Popover className="relative">
-      <div className="w-full bg-white">
-        <nav className="flex justify-between items-center wrapper">
-          {/* Left Nav */}
-          <div className="flex w-full lg:w-auto justify-between lg:justify-start lg:space-x-5 items-center flex-wrap">
-            {/* Logo */}
-            <div>
-              <Link href="/">
-                <a className="flex items-center space-x-2">
-                  <div className="w-9 h-9">
-                    <Image src={Logo} />
-                  </div>
-                  <span className="text-base1 font-semibold">Notion</span>
-                </a>
-              </Link>
+    <Disclosure as="nav" className="relative">
+      {({ open }) => (
+        <div className="w-full bg-white">
+          <div className="flex justify-between items-center wrapper">
+            {/* Left Nav */}
+            <div className="flex w-full lg:w-auto justify-between lg:justify-start lg:space-x-5 items-center flex-wrap">
+              {/* Logo */}
+              <div>
+                <Link href="/">
+                  <a className="flex items-center space-x-2">
+                    <div className="w-9 h-9">
+                      <Image src={Logo} />
+                    </div>
+                    <span className="text-base1 font-semibold">Notion</span>
+                  </a>
+                </Link>
+              </div>
+
+              {/* Mobile Menu Button */}
+              <div className="flex lg:hidden">
+                <Disclosure.Button>
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+              {/* NavBar Desktop */}
+              <div className="hidden lg:flex lg:w-auto justify-center">
+                <ul className="flex flex-col lg:flex-row lg:space-x-2">
+                  {navigation.map((item) => (
+                    <li>
+                      <Link href={item.href}>
+                        <a
+                          key={item.name}
+                          className={classNames(
+                            item.current
+                              ? "text-base font-medium text-gray-800 px-2 py-1 bg-gray-100 hover:bg-gray-100 rounded transition-colors"
+                              : "text-base font-medium text-gray-800 px-2 py-1 hover:bg-gray-100 rounded transition-colors"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            {/* Mobile Nav */}
-            <div className="flex lg:hidden">
-              <Popover.Button>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-7 w-7"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </Popover.Button>
+
+            {/* Right Nav */}
+            <div className="hidden lg:flex">
+              <button className="btn btn-primary ml-4">Try Notion free</button>
             </div>
-            {/* Nav Items */}
-            <div className="hidden lg:flex justify-center">
-              <ul className="flex flex-col lg:flex-row lg:space-x-2">
-                <NavItem href="/product" text="Product" />
-                <NavItem href="/solutions" text="Solutions" />
-                <NavItem href="/resources" text="Resources" />
-                <NavItem href="/download" text="Download" />
-                <NavItem href="/pricing" text="Pricing" />
-              </ul>
-            </div>
-            {/* Nav Mobile */}
-            <Popover.Panel className="flex w-full lg:hidden justify-center">
-              <ul className="flex flex-col lg:flex-row lg:space-x-2">
-                <NavItem href="/product" text="Product" />
-                <NavItem href="/solutions" text="Solutions" />
-                <NavItem href="/resources" text="Resources" />
-                <NavItem href="/download" text="Download" />
-                <NavItem href="/pricing" text="Pricing" />
-              </ul>
-            </Popover.Panel>
           </div>
-          {/* Right Nav */}
-          <div className="hidden lg:flex">
-            <ul className="flex space-x-2 items-center">
-              <NavItem href="/contact" text="Contact sales" />
-              <div className="nav-divider bg-gray-200"></div>
-              <NavItem href="/login" text="Log in" />
-              <button className="btn btn-primary">Try Notion free</button>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </Popover>
+
+          {/* NavBar Mobile */}
+          <Disclosure.Panel>
+            <div className="flex w-full lg:hidden justify-center text-center border-b">
+              <Transition
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opac  ity-0"
+              >
+                <ul className="space-y-4 pb-5">
+                  {navigation.map((item) => (
+                    <li>
+                      <Link href={item.href}>
+                        <a
+                          key={item.name}
+                          className={classNames(
+                            item.current
+                              ? "text-base font-medium text-gray-800 px-2 py-1 bg-gray-100 hover:bg-gray-100 rounded transition-colors"
+                              : "text-base font-medium text-gray-800 px-2 py-1 hover:bg-gray-100 rounded transition-colors"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </Transition>
+            </div>
+          </Disclosure.Panel>
+        </div>
+      )}
+    </Disclosure>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import SpotifyLogo from "../public/images/spotify.png";
 import SlackLogo from "../public/images/slack.png";
 import SpotContextImg from "../public/images/spot-context.png";
 import ContextTileImg from "../public/images/context-tile.png";
+import Fade from "react-reveal/Fade";
 
 export default function Home() {
   return (
@@ -27,47 +28,51 @@ export default function Home() {
       </div>
 
       {/* Hero */}
+
       <div className="mt-20">
         <div className="wrapper">
           <div className="text-center lg:text-left lg:py-20 grid grid-cols-2 gap-4">
-            <div className="w-full order-last col-span-2 lg:order-first lg:col-span-1 max-w-xs md:max-w-md xl:max-w-none mx-auto lg:mx-0">
-              <h1>One workspace. Every team.</h1>
-              <p className="mt-4 lg:max-w-md subheader">
-                We’re more than a doc. Or a table. Customize Notion to work the
-                way you do.
-              </p>
-              <button className="btn btn-lg btn-primary mt-6 mx-auto lg:mx-0">
-                Try Notion free
-              </button>
-              <div className="mt-10">
-                <span className="uppercase text-xs font-normal text-gray-400">
-                  Trusted by teams at
-                </span>
-                <div className="flex space-x-5 mt-3 items-center">
-                  <div className="logos-wrapper">
-                    <Image src={LoomLogo} />
-                  </div>
-                  <div className="logos-wrapper">
-                    <Image src={PixarLogo} />
-                  </div>
-                  <div className="logos-wrapper">
-                    <Image src={IBMLogo} />
-                  </div>
-                  <div className="logos-wrapper">
-                    <Image src={NikeLogo} />
-                  </div>
-                  <div className="logos-wrapper">
-                    <Image src={SpotifyLogo} />
-                  </div>
-                  <div className="logos-wrapper">
-                    <Image src={SlackLogo} />
+            <Fade bottom distance="20px">
+              <div className="w-full order-last col-span-2 lg:order-first lg:col-span-1 max-w-xs md:max-w-md xl:max-w-none mx-auto lg:mx-0">
+                <h1>One workspace. Every team.</h1>
+                <p className="mt-4 lg:max-w-md subheader">
+                  We’re more than a doc. Or a table. Customize Notion to work
+                  the way you do.
+                </p>
+                <button className="btn btn-lg btn-primary mt-6 mx-auto lg:mx-0">
+                  Try Notion free
+                </button>
+                <div className="mt-10">
+                  <span className="uppercase text-xs font-normal text-gray-400">
+                    Trusted by teams at
+                  </span>
+                  <div className="flex space-x-5 mt-3 items-center">
+                    <div className="logos-wrapper">
+                      <Image src={LoomLogo} />
+                    </div>
+                    <div className="logos-wrapper">
+                      <Image src={PixarLogo} />
+                    </div>
+                    <div className="logos-wrapper">
+                      <Image src={IBMLogo} />
+                    </div>
+                    <div className="logos-wrapper">
+                      <Image src={NikeLogo} />
+                    </div>
+                    <div className="logos-wrapper">
+                      <Image src={SpotifyLogo} />
+                    </div>
+                    <div className="logos-wrapper">
+                      <Image src={SlackLogo} />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="w-full col-span-2 max-w-xs md:max-w-lg lg:max-w-none lg:col-span-1 mx-auto items-center">
-              <Image src={HeroImg} />
-            </div>
+
+              <div className="w-full col-span-2 max-w-xs md:max-w-lg lg:max-w-none lg:col-span-1 mx-auto items-center">
+                <Image src={HeroImg} />
+              </div>
+            </Fade>
           </div>
         </div>
       </div>
@@ -76,24 +81,58 @@ export default function Home() {
         <div className="wrapper">
           <div className="grid grid-cols-3 gap-10">
             <div className="flex items-center col-span-3 lg:col-span-1">
-              <div className="space-y-4  ">
-                <Image
-                  className="object-contain"
-                  width={80}
-                  height={65}
-                  src={SpotContextImg}
-                />
-                <h2>Never ask “What’s the context?” again</h2>
-                <p className="subheader">
-                  Stale wikis aren't helpful. Neither are floating docs. In
-                  Notion, your daily work and knowledge live side by side — so
-                  you never lose context.
-                </p>
+              <Fade left distance="20px">
+                <div className="space-y-4  ">
+                  <Image
+                    className="object-contain"
+                    width={80}
+                    height={65}
+                    src={SpotContextImg}
+                  />
+                  <h2>Never ask “What’s the context?” again</h2>
+                  <p className="subheader">
+                    Stale wikis aren't helpful. Neither are floating docs. In
+                    Notion, your daily work and knowledge live side by side — so
+                    you never lose context.
+                  </p>
+                </div>
+              </Fade>
+            </div>
+            <Fade right distance="20px">
+              <div className="col-span-3 lg:col-span-2">
+                <Image className="rounded-xl" src={ContextTileImg} />
               </div>
+            </Fade>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="wrapper">
+          <div className="grid grid-cols-3 gap-10">
+            <div className="flex items-center col-span-3 lg:col-span-1">
+              <Fade left distance="20px">
+                <div className="space-y-4  ">
+                  <Image
+                    className="object-contain"
+                    width={80}
+                    height={65}
+                    src={SpotContextImg}
+                  />
+                  <h2>Never ask “What’s the context?” again</h2>
+                  <p className="subheader">
+                    Stale wikis aren't helpful. Neither are floating docs. In
+                    Notion, your daily work and knowledge live side by side — so
+                    you never lose context.
+                  </p>
+                </div>
+              </Fade>
             </div>
-            <div className="col-span-3 lg:col-span-2">
-              <Image className="rounded-xl" src={ContextTileImg} />
-            </div>
+            <Fade right distance="20px">
+              <div className="col-span-3 lg:col-span-2">
+                <Image className="rounded-xl" src={ContextTileImg} />
+              </div>
+            </Fade>
           </div>
         </div>
       </div>
